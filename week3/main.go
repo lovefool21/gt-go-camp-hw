8479d46581baa4b6af9d4365f35933d48063f6c0
nuccky <nuccky@nucckydeMini.lan>
2021-05-10 21:12:11 +0800

    week3-1

--- week3/main.go
+++ week3/main.go
@@ -8,6 +8,10 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
+//reference https://pkg.go.dev/golang.org/x/sync/errgroup#WithContext
+//笔记 errorgroup 的作用 多个goroutine执行时 记录各个groutine发生的错误
+//每个子任务遇到错误时都应该抛出，g.Wait()等待所有子任务运行完成 最终返回结果集和错误信息
+
 func main() {
 	var g errgroup.Group
 
