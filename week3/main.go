65a305c4c04845c53dd8fe6b1efd590427117877
nuccky <nuccky@nucckydeMini.lan>
2021-05-10 20:46:27 +0800

    week3

--- week3/main.go
+++ week3/main.go
@@ -1,35 +1,40 @@
 package main
 
 import (
-    "fmt"
-	"net/http"
+	"errors"
+	"fmt"
+	"time"
 
-    "golang.org/x/sync/errgroup"
+	"golang.org/x/sync/errgroup"
 )
-
-//参考文档 https://godoc.org/golang.org/x/sync/errgroup
 
 func main() {
 	var g errgroup.Group
-	var urls = []string{
-		"http://www.baidu.com/",
-		"http://www.hao123.com/",
-		"http://www.somestupidname.com/",
-	}
-	for _, url := range urls {
-		// Launch a goroutine to fetch the URL.
-		url := url // https://golang.org/doc/faq#closures_and_goroutines
-		g.Go(func() error {
-			// Fetch the URL.
-			resp, err := http.Get(url)
-			if err == nil {
-				resp.Body.Close()
-			}
-			return err
-		})
-	}
-	// Wait for all HTTP fetches to complete.
+
+	// 启动第一个子任务,它执行成功
+	g.Go(func() error {
+		time.Sleep(5 * time.Second)
+		fmt.Println("exec #1")
+		// return errors.New("failed to exec #1")
+		return nil
+	})
+	// 启动第二个子任务，它执行失败
+	g.Go(func() error {
+		time.Sleep(10 * time.Second)
+		fmt.Println("exec #2")
+		return errors.New("failed to exec #2")
+	})
+
+	// 启动第三个子任务，它执行成功
+	g.Go(func() error {
+		time.Sleep(15 * time.Second)
+		fmt.Println("exec #3")
+		return nil
+	})
+	// 等待三个任务都完成
 	if err := g.Wait(); err == nil {
-		fmt.Println("Successfully fetched all URLs.")
+		fmt.Println("Successfully exec all")
+	} else {
+		fmt.Println("failed:", err)
 	}
 }
